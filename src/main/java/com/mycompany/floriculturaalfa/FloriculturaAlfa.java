9096b60a7ac735663a7c4c0f39e4c21e76ecb754
RafaMachad <rmr.eu@hotmail.com.br>
2023-04-13 15:36:24 -0300

    upload

---
+++ src/main/java/com/mycompany/floriculturaalfa/FloriculturaAlfa.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.floriculturaalfa;
+
+
+public class FloriculturaAlfa {
+
+    public static void main(String[] args) {
+
+        TelaInicial telaPrincipal = new TelaInicial();
+        telaPrincipal.setVisible(true);
+        
+        
+        
+        
+    }
+}
---
+++ src/main/java/com/mycompany/floriculturaalfa/Produtos.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.floriculturaalfa;
+
+/**
+ *
+ * @author Admin
+ */
+public class Produtos {
+    
+    private String nome;
+    private double preco;
+    private int quantidadeEmEstoque;
+
+    public Produtos(String nome, double preco, int quantidadeEmEstoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    // Métodos getters e setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+}
+
